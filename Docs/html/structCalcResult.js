c961d5a8085e76405f89923750b7abb6ef6edd0c
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-14 00:52:52 +0300

    Add files via upload

---
+++ Docs/html/structCalcResult.js
@@ -0,0 +1,5 @@
+var structCalcResult =
+[
+    [ "x", "structCalcResult.html#a2be7fadf793c0c1e01f0a39d778c11e0", null ],
+    [ "y", "structCalcResult.html#a46f1ca01aafdaa79e16957df2e3183c7", null ]
+];
