cbbbaa7a9bce3705da151eefd196913afa95f24e
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-14 00:51:29 +0300

    Add files via upload

---
+++ Docs/html/annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "AuthManager", "classAuthManager.html", "classAuthManager" ],
+    [ "CalcResult", "structCalcResult.html", "structCalcResult" ],
+    [ "Server", "classServer.html", "classServer" ],
+    [ "VerificationContext", "structVerificationContext.html", "structVerificationContext" ]
+];
