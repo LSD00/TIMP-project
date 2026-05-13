e19e17b6a521d67266d4990dc0845772454994d6
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-14 00:40:44 +0300

    Add files via upload

---
+++ client/Docs/html/classgraph.js
@@ -0,0 +1,11 @@
+var classgraph =
+[
+    [ "graph", "classgraph.html#a797e6f418c9ce21878678a3f4847c2b0", null ],
+    [ "~graph", "classgraph.html#aeb62eaf197cdcb4800fa016eebc3d55a", null ],
+    [ "on_horizontalSlider_2_sliderMoved", "classgraph.html#a6c7790f277ba4095be7606101db93daa", null ],
+    [ "on_horizontalSlider_3_sliderMoved", "classgraph.html#ade35ac8921790db722def803a594b0b0", null ],
+    [ "on_horizontalSlider_sliderMoved", "classgraph.html#ab2fed8e9e7551677a453039eb608efc2", null ],
+    [ "on_pushButton_clicked", "classgraph.html#aae15bc16bda368912aeba29d8224b6b6", null ],
+    [ "printgraph", "classgraph.html#a9084109811dd9d632d92d5ec7f08d1ab", null ],
+    [ "ui", "classgraph.html#ad7c43af2f936234ded728bfbd7ae9407", null ]
+];
---
+++ client/Docs/html/classregistration.js
@@ -0,0 +1,11 @@
+var classregistration =
+[
+    [ "registration", "classregistration.html#a18fb1e1dad535252deb3b46d858f0388", null ],
+    [ "~registration", "classregistration.html#a810017b2a89d603f4cf11a0beb9eac4b", null ],
+    [ "on_lineEdit_2_textChanged", "classregistration.html#a1df7ac2d16ee73b3e59d442372408581", null ],
+    [ "on_lineEdit_3_textChanged", "classregistration.html#aa08c91ee34889a27acca0cf31c9a8292", null ],
+    [ "on_lineEdit_textChanged", "classregistration.html#a2c668dbe2be08dd568359f66571f52fb", null ],
+    [ "on_pushButton_2_clicked", "classregistration.html#ae9474d88b3629a36af6be4f6309d5bf7", null ],
+    [ "on_pushButton_clicked", "classregistration.html#a43a40ec5598f757d0163d5ffda0d3275", null ],
+    [ "ui", "classregistration.html#a233165f565b3f9ee665e8cdbdf145e13", null ]
+];
