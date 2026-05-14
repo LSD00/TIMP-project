f478d3a0a17b09277e804c851c808663dae195d6
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-14 23:23:11 +0300

    Add files via upload

---
+++ Docs/html/AuthManagerDocs_8h.js
@@ -0,0 +1,19 @@
+var AuthManagerDocs_8h =
+[
+    [ "VerificationContext", "structVerificationContext.html", "structVerificationContext" ],
+    [ "AuthManager", "classAuthManager.html", "classAuthManager" ],
+    [ "AuthResult", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fce", [
+      [ "Success", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea505a83f220c02df2f85c3810cd9ceb38", null ],
+      [ "ConnectionError", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea094a6f6b0868122a9dd008cb91c083e4", null ],
+      [ "InvalidEmail", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea45d3c76b79a4795dae38fb5d5b48ad45", null ],
+      [ "WeakPassword", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea460899c942673a93b86a81ca6297cd36", null ],
+      [ "UserAlreadyExists", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea28ec83712271c6d109f6eac45c04fc71", null ],
+      [ "UserNotFound", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea5e349b2189e271e32f633c077d4ff028", null ],
+      [ "DatabaseError", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fceaa31437013b94ae5b39a818c6defac7ea", null ],
+      [ "InvalidCredentials", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea3121e825c13ba377fb5adb741201c401", null ],
+      [ "CodeExpired", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea8efc205b664814d3458d9457a330ee78", null ],
+      [ "InvalidCode", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea54811ceb822294b83e9ded0838e73c02", null ],
+      [ "TooManyAttempts", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fceaa7ef31aab8d88f6735da209bb411fd40", null ],
+      [ "CodeNotVerified", "AuthManagerDocs_8h.html#a87ba9bd5edfd749f4729620dbadd5fcea3da277f7b49fb7aca46d67a5600011f2", null ]
+    ] ]
+];
---
+++ Docs/html/classServer.js
@@ -0,0 +1,12 @@
+var classServer =
+[
+    [ "Server", "classServer.html#aaf98d5194faee831c6340cc736b9b879", null ],
+    [ "handleRequest", "classServer.html#a765e4ffe32ffff676ba9e523ab7fcdaa", null ],
+    [ "incomingConnection", "classServer.html#aa229a1587514197693274aa0d589344b", null ],
+    [ "onClientDisconnected", "classServer.html#a8afa35c8257d78be38596587e469bea3", null ],
+    [ "onReadyRead", "classServer.html#ac67d866fb14c4644acd1098bacfadeee", null ],
+    [ "sendAuthResponse", "classServer.html#ad0a7ffb50db36752ca6d4d43eb574562", null ],
+    [ "sendResponse", "classServer.html#a5c38927d999bbb4155872da0974d8717", null ],
+    [ "startServer", "classServer.html#a345934ddad7b5a98d058801aa3edcb02", null ],
+    [ "authenticatedClients", "classServer.html#a2a54dfaaa52018da9d2537f68ded1026", null ]
+];
