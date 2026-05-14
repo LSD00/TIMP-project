8fcfadf8977ec8d06cfba2c7b18e2bda544ac274
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-15 02:49:05 +0300

    Add files via upload

---
+++ docs/server/html/structVerificationContext.js
@@ -0,0 +1,7 @@
+var structVerificationContext =
+[
+    [ "attempts", "structVerificationContext.html#a6134b5c5808f092850ef8c2c0d803e85", null ],
+    [ "code", "structVerificationContext.html#a58b0df7ea3df092cc1c8142b730036bb", null ],
+    [ "expiresAt", "structVerificationContext.html#a1b4631d5864451459a3014a59040ae98", null ],
+    [ "isVerified", "structVerificationContext.html#a871bde05deb61ac3f7ce6328df614b68", null ]
+];
