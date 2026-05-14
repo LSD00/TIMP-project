8dfa65a5d3e49744db1d66ee6872a1c5c3240054
DR-ROSTI <rustytechnical@yandex.ru>
2026-05-14 23:24:41 +0300

    Add files via upload

---
+++ Docs/html/ServerDocs_8h.js
@@ -0,0 +1,4 @@
+var ServerDocs_8h =
+[
+    [ "Server", "classServer.html", "classServer" ]
+];
---
+++ Docs/html/hierarchy.js
@@ -0,0 +1,11 @@
+var hierarchy =
+[
+    [ "CalcResult", "structCalcResult.html", null ],
+    [ "QObject", null, [
+      [ "AuthManager", "classAuthManager.html", null ]
+    ] ],
+    [ "QTcpServer", null, [
+      [ "Server", "classServer.html", null ]
+    ] ],
+    [ "VerificationContext", "structVerificationContext.html", null ]
+];
